06ea96d39a41e6bd63d54966e512793211642503
skyRocket23 <gitBelwal23@gmail.com>
2022-01-17 12:56:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
+import About from './components/About';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils2"/>
+    <Navbar title="TextUtils2" mode={mode} toggleMode={toggleMode}/>
     <div className="container">
-      <TextForm heading="Text-Analyser"/>
+      {/* <About/> */}
+        <TextForm heading="Text-Analyser" mode={mode}/>
     </div>
       
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,6 +19,18 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleCopy=()=>{
+        var text=document.getElementById("myBox");
+        text.select();
+        // text.setSelectionRange(0,9999);;
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces=()=>{
+        let newText=text.split(/[]+/);
+        setText(newText.join(" "))
+    }
+
     const handleWordLength = () => {
         let r = 0.008 * text.split(" ").length;
         let newText='';
@@ -48,13 +60,15 @@ export default function TextForm(props) {
             <div className='container'>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="12"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode=== 'light' ? 'green':'white'}} id="myBox" rows="12"></textarea>
                 </div>
 
                 <button className="btn btn-success mx-2" onClick={handleUpClick}>Uppercase</button>
                 <button className="btn btn-info mx-2" onClick={handleDownClick}>Lowercase</button>
                 <button className="btn btn-warning mx-2" onClick={handleWordLength}>Text Strength</button>
                 <button className="btn btn-danger mx-2" onClick={handleClearText}>Clear</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy</button>
+                {/* <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>ExtraSpaces</button> */}
                 
             </div>
 
